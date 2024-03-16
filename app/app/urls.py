3ec6d993114d2f3a905d93eed240a81205666dc4
vasin yeagorr <anyaivantsova22@gmail.com>
2024-03-16 17:25:49 +0100

    remove venv

--- app/app/urls.py
+++ app/app/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view()),
     path('profile/', views.ProfileView.as_view()),
     path('wounds/', views.WoundUploadView.as_view()),
+    path('cases/', views.CaseView.as_view()),
     path('accounts/', include('rest_registration.api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
